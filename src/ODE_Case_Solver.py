8d46fe9fb1630c7ab0146cc478ae22e34e62e168
Joshua Oyedemi <josh.oyed@gmail.com>
2024-12-03 22:17:08 -0700

    Files updated

---
+++ src/ODE_Case_Solver.py
@@ -0,0 +1,16 @@
+import numpy as np # type: ignore
+
+#Parameters:
+
+phi = 51.0486 # Latitude in Calgary,Alberta.
+g0 = 9.811636 # measured in  m/s^2.
+dg_dz = 3.086*10-6 #g′ ≈ 0.3086 mGal/m where 1 Gal = 1 cm/s2, so in SI units, g′ ≈ (m/s2)/m.
+rho_steel = 7800 # measured in  kg/m3.
+d = 0.015 # density converted to m from cm (1.5cm).
+μ_air =  1.827*10-5 # measured in kg/(m⋅s).
+
+
+
+
+
+#Returns
